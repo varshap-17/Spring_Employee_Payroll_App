408d62cc5ce42a6419454672b1db31db5f60e4d2
varshap-17 <varshap630@gmail.com>
2022-09-27 10:33:31 +0530

    Interface class

---
+++ src/main/java/com/example/employeepayroll/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.employeepayroll.repository;
+
+import com.example.employeepayroll.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
