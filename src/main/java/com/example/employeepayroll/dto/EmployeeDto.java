ede9f51d6b07add32c0db06de7fc652a68aeba64
varshap-17 <varshap630@gmail.com>
2022-09-29 15:29:39 +0530

    dto class

---
+++ src/main/java/com/example/employeepayroll/dto/EmployeeDto.java
@@ -0,0 +1,10 @@
+package com.example.employeepayroll.dto;
+
+public class EmployeeDto {
+    public String name;
+    public String salary;
+    public EmployeeDto(String name,String salary){
+        this.name=name;
+        this.salary=salary;
+    }
+}
