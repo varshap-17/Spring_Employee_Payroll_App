922826f01d526b9fd85144da48d2f7b3157345fb
varshap-17 <varshap630@gmail.com>
2022-09-30 14:59:21 +0530

    Add validation to Name field so the rest call can be validated

--- src/main/java/com/example/employeepayroll/dto/EmployeeDto.java
+++ src/main/java/com/example/employeepayroll/dto/EmployeeDto.java
@@ -1,7 +1,12 @@
 package com.example.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 public class EmployeeDto {
+    @NotEmpty(message="Employee name can not be null")
     public String name;
+    @Min(value = 500,message = "min wage should be more than 500")
     public String salary;
     public EmployeeDto(String name,String salary){
         this.name=name;
