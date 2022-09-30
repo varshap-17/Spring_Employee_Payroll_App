9eec578f9cf3a252e705914a0781ee4702773f25
varshap-17 <varshap630@gmail.com>
2022-09-30 15:13:52 +0530

    Provide User friendly Error Response in case Validation fails

---
+++ src/main/java/com/example/employeepayroll/exception/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.employeepayroll.exception;
+
+import com.example.employeepayroll.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
+        List<String> errMesg=errorList.stream().map(ObjErr->ObjErr.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDto responseDto=new ResponseDto("Exception while processing REST Request",errMesg);
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+}
