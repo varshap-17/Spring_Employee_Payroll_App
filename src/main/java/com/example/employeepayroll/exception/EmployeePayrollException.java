096ce7cc58f14841ea680f0d5ad3d866d2d03b49
varshap-17 <varshap630@gmail.com>
2022-10-01 19:04:56 +0530

    employee payroll exception class

---
+++ src/main/java/com/example/employeepayroll/exception/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.example.employeepayroll.exception;
+
+
+public class EmployeePayrollException extends RuntimeException{
+    public EmployeePayrollException(String message){
+            super(message);
+    }
+}
