f5684c57aabd5f0a41ad7f904060c44a46572134
varshap-17 <varshap630@gmail.com>
2022-10-03 13:23:51 +0530

    employee controller class

--- src/main/java/com/example/employeepayroll/controller/Empcontroller.java
+++ src/main/java/com/example/employeepayroll/controller/Empcontroller.java
@@ -1,14 +1,19 @@
 package com.example.employeepayroll.controller;
 
+import com.example.employeepayroll.dto.EmpDto;
+import com.example.employeepayroll.dto.ResponseDto;
 import com.example.employeepayroll.model.Employee;
 import com.example.employeepayroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+ @RestController
 @RequestMapping("/emp")
 
 public class Empcontroller {
@@ -16,25 +21,40 @@ public class Empcontroller {
     EmployeeService employeeService;
 
     @PostMapping("/postdata")
-    public Employee input(@RequestBody Employee employee) {
-        Employee empData = employeeService.postdata(employee);
-        return empData;
+    public ResponseEntity<ResponseDto> postData(@Valid @RequestBody EmpDto empDto){
+        Employee employee=null;
+        employee=employeeService.postdata(empDto);
+        ResponseDto responseDto=new ResponseDto("saved data successfully",employee);
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
     }
     @GetMapping("/getall")
     public List<Employee> getAll(){
         return employeeService.getAllData();
     }
+
     @PutMapping("/edit/{id}")
-    public Employee editbyid(@PathVariable Integer id,@RequestBody Employee employee){
-        return employeeService.updateDataById(id, employee);
+    public ResponseEntity<ResponseDto> updateById(@Valid @PathVariable Integer id,@RequestBody EmpDto empDto){
+        Employee employee=employeeService.updateDataById(id,empDto);
+        ResponseDto responseDto=new ResponseDto("Updated data successfully",employee);
+        return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
     @GetMapping("/getbyid{id}")
-    public Optional<Employee> getById(@PathVariable Integer id){
-        return employeeService.findById(id);
+    public ResponseEntity<ResponseDto> find(@PathVariable Integer id){
+        Optional<Employee> employee=null;
+        employee=employeeService.findById(id);
+        ResponseDto responseDto=new ResponseDto("get call for id successfully",employee);
+        return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
     @DeleteMapping("/delete/{id}")
-    public String deletebyid(@PathVariable Integer id){
+    public ResponseEntity<ResponseDto> delete(@PathVariable Integer id){
         employeeService.deletedata(id);
-        return "Employee with id:"+id+" deleted";
+        ResponseDto responseDto=new ResponseDto("deleted id:"+id+" successfully",id);
+        return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
+    }
+    @GetMapping("/firstname/{firstName}")
+     public ResponseEntity<ResponseDto> getempbyname(@PathVariable String firstName){
+        List<Employee> employee=employeeService.getEmployeeByName(firstName);
+        ResponseDto responseDto=new ResponseDto("get employee details from name",employee);
+        return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
 }
