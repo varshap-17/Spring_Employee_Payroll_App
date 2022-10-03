5358f7fe3efef48ac14f7a12bcf07b8160cddf14
varshap-17 <varshap630@gmail.com>
2022-10-03 13:32:03 +0530

    repository class

--- src/main/java/com/example/employeepayroll/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeepayroll/repository/EmployeeRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     @Query(value = "select * from employee where first_name=:firstName",nativeQuery=true)
     List<Employee> findEmployeeByFirstName(String firstName);
+    @Query(value = "SELECT * from employee where department=:department",nativeQuery = true)
+    List<Employee> findEmployeesByDepartment(String department);
 }
